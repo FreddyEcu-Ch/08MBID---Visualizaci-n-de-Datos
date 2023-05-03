dbeef357d59711fa8c9bbb6f5494eb18df0458bc
FreddyEcu-Ch <57779200+FreddyEcu-Ch@users.noreply.github.com>
2023-05-03 15:34:20 -0500

    1 file modified

--- index.js
+++ index.js
@@ -50,3 +50,25 @@ d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Activi
     .draw(); 
 
 });
+
+d3.csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv", function(error, data) 
+
+{ 
+
+    var visualization = d3plus.viz()
+
+      .container("#viz3")
+
+      .data([{"year": 1991, "name":"alpha", "value": 15}, {"year": 1992, "name":"alpha", "value": 20},]) 
+
+      .type("bar")
+
+      .id("name")
+
+      .x("year")
+
+      .y("value")
+
+      .draw()
+
+});
